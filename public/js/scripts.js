557615bfb13e1c82a3163b0cd39544f377c423f1
Brianna Deleasa <bdeleasa@gmail.com>
2015-05-28 14:34:19 -0400

    Setting the threshold for loading the images.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,13 +3,16 @@
 (function($) {
 
     // Enable lazy loading for image tags
+    // @todo add a settings page for these params
     $('img.lazy').lazyload({
+        threshold : 200,
         effect : 'fadeIn'
     });
 
     // Enables lazy loading on Ajax Requests
     $(document).ajaxStop(function(){
         $('img.lazy').lazyload({
+            threshold : 200,
             effect: 'fadeIn'
         }).removeClass('lazy');
     });
